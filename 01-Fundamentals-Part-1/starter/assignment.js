5478f869f5bbe2ffabd9daced909a46a7f13d3b4
zacharyk30 <58117892+zacharyk30@users.noreply.github.com>
2024-03-07 15:33:05 -0600

    Values and Vars

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,6 @@
+let country = "United States";
+let continent = "North America";
+let population = 365;
+
+console.log("Country: " + country + "/n Con: " + continent
+    + "/n pop: " + population);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,9 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 
 let y = 40 + 8 + 23 - 10;
 console.log(y);
+
+let fName = "Zak";
+console.log(fName);
+
+
